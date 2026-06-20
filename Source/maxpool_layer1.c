7634f89d017bf364f8d8e9f1d55e0a7bc32170cf
Trịnh Minh Khanh <128967943+KankDz@users.noreply.github.com>
2026-06-20 10:25:59 +0700

    Update maxpool_layer1.c

--- Source/maxpool_layer1.c
+++ Source/maxpool_layer1.c
@@ -6,38 +6,33 @@ void maxpool_layer1(
     data_t output[POOL1_ROWS][POOL1_COLS][C1]
 ){
     Channel:
-    for(int i = 0; i < C1; i++){
+    for(int ch = 0; ch < C1; ch++){
 
         Row:
-        for(int j = 0; j < CONV1_ROWS; j += POOL_SIZE){
+        for(int out_row = 0; out_row < POOL1_ROWS; out_row++){
 
             Col:
-            for(int k = 0; k < CONV1_COLS; k += POOL_SIZE){
+            for(int out_col = 0; out_col < POOL1_COLS; out_col++){
 
-                data_t maxvalue = 0;
+                int in_row = out_row * POOL_SIZE;
+                int in_col = out_col * POOL_SIZE;
+                data_t maxvalue = input[in_row][in_col][ch];
 
-                int row_end =
-                    (j + POOL_SIZE < CONV1_ROWS)
-                    ? (j + POOL_SIZE)
-                    : CONV1_ROWS;
+                POOL_ROW:
+                for(int pool_row = 0; pool_row < POOL_SIZE; pool_row++){
 
-                int col_end =
-                    (k + POOL_SIZE < CONV1_COLS)
-                    ? (k + POOL_SIZE)
-                    : CONV1_COLS;
+                    POOL_COL:
+                    for(int pool_col = 0; pool_col < POOL_SIZE; pool_col++){
 
-                MAX_ROW:
-                for(int a = j; a < row_end; a++){
+                        data_t value =
+                            input[in_row + pool_row][in_col + pool_col][ch];
 
-                    MAX_COL:
-                    for(int b = k; b < col_end; b++){
-
-                        if(input[a][b][i] > maxvalue)
-                            maxvalue = input[a][b][i];
+                        if(value > maxvalue)
+                            maxvalue = value;
                     }
                 }
 
-                output[j/POOL_SIZE][k/POOL_SIZE][i] = maxvalue;
+                output[out_row][out_col][ch] = maxvalue;
             }
         }
     }
