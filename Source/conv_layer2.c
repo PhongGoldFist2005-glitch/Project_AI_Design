709abbc64684421eb366f2aebddd87a0625d375f
Trịnh Minh Khanh <128967943+KankDz@users.noreply.github.com>
2026-06-20 08:27:22 +0700

    Update conv_layer2.c

--- Source/conv_layer2.c
+++ Source/conv_layer2.c
@@ -1,9 +1,8 @@
 #include "tinyyolo_lite.h"
+#include "weights.h"
 
 void conv_layer2(
     data_t input [POOL1_ROWS][POOL1_COLS][C1],
-    data_t weight[KERNAL_SIZE][KERNAL_SIZE][C1][C2],
-    data_t bias  [C2],
     data_t output[CONV2_ROWS][CONV2_COLS][C2]
 )
 {
@@ -16,7 +15,7 @@ void conv_layer2(
         for (col = 0; col < CONV2_COLS; col++) {
             LOOP_OC:
             for (oc = 0; oc < C2; oc++) {
-                sum = bias[oc];
+                sum = b_conv2[oc];
                 LOOP_KR:
                 for (kr = 0; kr < KERNAL_SIZE; kr++) {
                     LOOP_KC:
@@ -24,7 +23,7 @@ void conv_layer2(
                         LOOP_IC:
                         for (ic = 0; ic < C1; ic++) {
                             sum += input[row+kr][col+kc][ic]
-                                 * weight[kr][kc][ic][oc];
+                                 * w_conv2[kr][kc][ic][oc];
                         }
                     }
                 }
