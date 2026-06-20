e59aeb7bfbe6a7e4f7a15c276f12d2d3722e3003
Trịnh Minh Khanh <128967943+KankDz@users.noreply.github.com>
2026-06-20 08:30:02 +0700

    Update tinyyolo_lite.c

--- Source/tinyyolo_lite.c
+++ Source/tinyyolo_lite.c
@@ -15,6 +15,8 @@
  */
 
 #include "tinyyolo_lite.h"
+#define WEIGHTS_DEFINE
+#include "weights.h"
 
 // ============================================================
 // Intermediate buffers (static to avoid stack overflow on HLS)
@@ -31,38 +33,18 @@ static data_t buf_det   [GRID_SIZE][GRID_SIZE][DET_OUT];
 //
 // Parameters:
 //   image    - Input image   [64][64][3]
-//   w_conv1  - Conv1 weights [3][3][3][C1]
-//   b_conv1  - Conv1 biases  [C1]
-//   w_conv2  - Conv2 weights [3][3][C1][C2]
-//   b_conv2  - Conv2 biases  [C2]
-//   w_conv3  - Conv3 weights [3][3][C2][C3]
-//   b_conv3  - Conv3 biases  [C3]
-//   w_det    - Det   weights [5][5][C3][DET_OUT]
-//   b_det    - Det   biases  [DET_OUT]
+//   weights  - Internal weights and biases from weights.h
 //   output   - Decoded boxes [GRID][GRID][BOX_SIZE][5+CLASSES]
 // ============================================================
 void tinyyolo_lite(
     data_t image  [INPUT_ROWS][INPUT_COLS][INPUT_CH],
-
-    data_t w_conv1[KERNAL_SIZE][KERNAL_SIZE][INPUT_CH][C1],
-    data_t b_conv1[C1],
-
-    data_t w_conv2[KERNAL_SIZE][KERNAL_SIZE][C1][C2],
-    data_t b_conv2[C2],
-
-    data_t w_conv3[KERNAL_SIZE][KERNAL_SIZE][C2][C3],
-    data_t b_conv3[C3],
-
-    data_t w_det  [KERNAL_DETECT][KERNAL_DETECT][C3][DET_OUT],
-    data_t b_det  [DET_OUT],
-
     data_t output [GRID_SIZE][GRID_SIZE][BOX_SIZE][OUTPUT_DECODED]
 )
 {
     // ----------------------------------------------------------
     // Stage 1: Conv Layer 1  (64x64x3 --> 62x62xC1)  + ReLU
     // ----------------------------------------------------------
-    conv_layer1(image, w_conv1, b_conv1, buf_conv1);
+    conv_layer1(image, buf_conv1);
 
     // ----------------------------------------------------------
     // Stage 2: MaxPool Layer 1  (62x62xC1 --> 31x31xC1)
@@ -72,7 +54,7 @@ void tinyyolo_lite(
     // ----------------------------------------------------------
     // Stage 3: Conv Layer 2  (31x31xC1 --> 29x29xC2)  + ReLU
     // ----------------------------------------------------------
-    conv_layer2(buf_pool1, w_conv2, b_conv2, buf_conv2);
+    conv_layer2(buf_pool1, buf_conv2);
 
     // ----------------------------------------------------------
     // Stage 4: MaxPool Layer 2  (29x29xC2 --> 14x14xC2)
@@ -82,12 +64,12 @@ void tinyyolo_lite(
     // ----------------------------------------------------------
     // Stage 5: Conv Layer 3  (14x14xC2 --> 12x12xC3)  + ReLU
     // ----------------------------------------------------------
-    conv_layer3(buf_pool2, w_conv3, b_conv3, buf_conv3);
+    conv_layer3(buf_pool2, buf_conv3);
 
     // ----------------------------------------------------------
     // Stage 6: Detection Head  (12x12xC3 --> 8x8xDET_OUT)
     // ----------------------------------------------------------
-    detection_head(buf_conv3, w_det, b_det, buf_det);
+    detection_head(buf_conv3, buf_det);
 
     // ----------------------------------------------------------
     // Stage 7: Decode  (8x8xDET_OUT --> 8x8xBOX_SIZEx(5+CLASSES))
